acc18d330bda749807e256ced56cad9333e2609b
Giuseppe <giuseppe.ibba@hotmail.com>
2017-12-01 14:18:34 +0000

    Added test code to guest.class properties

---
+++ guest.rb
@@ -0,0 +1,11 @@
+class Guest
+
+  attr_reader :name, :surname, :age
+
+  def initialize(name, surname, age)
+    @name = name
+    @surname = surname
+    @age = age
+  end
+
+end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -8,4 +8,18 @@ def setup
   @guest = Guest.new("Phil", "Collins", 35)
 end
 
+
+
+def test_guest_has_a_name()
+  assert_equal("Phil", @guest.name())
+end
+
+def test_guest_has_a_surname()
+  assert_equal("Collins", @guest.surname())
+end
+
+def test_guest_has_a_age
+  assert_equal(35, @guest.age)
+end
+
 end
