9ed44ef6c7cb99231cb58e6d3cdf4894417900c2
Giuseppe <giuseppe.ibba@hotmail.com>
2017-12-01 14:25:36 +0000

    added tests to room properties

---
+++ room.rb
@@ -0,0 +1,11 @@
+class Room
+
+  attr_reader :genre, :room_number
+
+  def initialize(genre, room_number)
+
+  @genre = genre
+  @room_number = room_number
+
+end
+end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,5 +7,15 @@ class TestRoom < MiniTest::Test
   def setup
 
     @room = Room.new("Rock" , 3)
+  end
+
+
+
+  def test_room_has_a_name()
+    assert_equal("Rock", @room.genre())
+  end
+
+  def test_room_has_a_room_number()
+  assert_equal(3, @room.room_number)
 end
 end
